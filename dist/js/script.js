90589a04993762a8528c29555a465b75a9c5ffbb
Marina <m_ilyushina@hotmail.com>
2022-01-27 22:05:19 +0100

    Script

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      counters = document.querySelectorAll('.skills__scores-counter'),
+      lines =document.querySelectorAll('.skills__scores-line span'),
+      sidepanel = document.querySelectorAll('.sidepanel');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,5 +11,8 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+counters.forEach((item, i) => {
+    lines[i].getElementsByClassName.width = item.innerHTML;
 });
 
