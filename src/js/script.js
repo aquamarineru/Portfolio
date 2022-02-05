6158ccd00fcfae5a9b2c4630424c2359be41b7fd
Marina <m_ilyushina@hotmail.com>
2022-02-05 12:10:47 +0100

    Photo&sidepanel

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,17 @@ counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+$(document).ready(function(){
+    $(window).scroll(function(){  
+        if ($(window).scrollTop() > 130 ){
+            $(".sidepanel__link-svg").css("color", "rgb(6, 35, 4)");
+			$(".sidepanel__divider").css("background-color", "rgb(6, 35, 4)");
+			$(".sidepanel__text").css("color", "rgb(6, 35, 4)");
+		}  else {
+			$(".sidepanel__link-svg").css("color", "white");
+			$(".sidepanel__divider").css("background-color", "white");
+			$(".sidepanel__text").css("color", "white");
+		} 
+    });
+});  
+
